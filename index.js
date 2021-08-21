200735513aed211046d071298a996a1ea851e7a3
Galatia <ptrivedius@aol.com>
2021-08-21 18:06:31 -0400

    to add files and start markdown and index code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
@@ -38,7 +40,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('README.md', questions, (err) {
+  if (err) throw err; 
+
+  else {
+    generateMarkdown();
+    console.log("README.md generated!");
+  }
+});
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  if (license) {
+    return;
+  }
+  else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license) {
+   return;
+ }
+  else {
+    return "";
+  }
 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+  if (license) {
+    return;
+  }
+  else {
+    return "";
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# <Your-Project-Title>
-  ## Description
+  return `# ${data.title}
+  ## ${data.description}
+
+  ## License
+  ## Badges
   ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
